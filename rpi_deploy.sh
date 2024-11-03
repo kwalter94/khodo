c10f6aec04c8b276bfe707dfbca98fc5ce6cd395
Walter Kaunda <walterkaunda@gmail.com>
2024-11-03 22:07:38 +0200

    Add auto-tagging of rpi images

--- rpi_deploy.sh
+++ rpi_deploy.sh
@@ -9,15 +9,16 @@ if [ -z $1 ]; then
 fi
 
 server=$1
+version=$(git describe --tags)
 
 echo "Building image"
-docker buildx build --platform linux/arm64/v8 --load -f docker/Dockerfile -t kwalter94/khodo:latest .
-docker save -o khodo.img kwalter94/khodo:latest
+docker buildx build --platform linux/arm64/v8 --load -f docker/Dockerfile -t kwalter94/khodo:latest -t kwalter94/khodo:$version .
+docker save -o khodo-$version.img kwalter94/khodo:$version kwalter94/khodo:latest
 
 echo "Copying image to pi"
 ssh $server "[ -d khodo ] || mkdir khodo"
-rsync --progress khodo.img docker-compose-prod.yml $server:khodo/
-ssh $server "cd khodo && docker load -i khodo.img && docker-compose -f docker-compose-prod.yml restart"
-rm khodo.img
+rsync --progress khodo-$version.img docker-compose-prod.yml $server:khodo/
+ssh $server "cd khodo && docker load -i khodo-$version.img && TAG=$version docker-compose -f docker-compose-prod.yml up --wait --detach && rm khodo-$version.img"
+rm khodo-$version.img
 
 echo "Update done"
