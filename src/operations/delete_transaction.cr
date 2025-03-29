90b2e94358887045e16a071fa6c3ecaa95ae58a5
Walter Kaunda <walterkaunda@gmail.com>
2025-03-29 23:00:31 +0200

    Add account balance transaction reversals

--- src/operations/delete_transaction.cr
+++ src/operations/delete_transaction.cr
@@ -1,4 +1,8 @@
 class DeleteTransaction < Transaction::DeleteOperation
   # Read more on deleting records
   # https://luckyframework.org/guides/database/deleting-records
+
+  after_delete do |tx|
+    SaveAccountBalance.reverse_transaction(tx)
+  end
 end
--- src/operations/save_account_balance.cr
+++ src/operations/save_account_balance.cr
@@ -41,6 +41,55 @@ class SaveAccountBalance < AccountBalance::SaveOperation
     end
   end
 
+  def self.reverse_transaction(tx : Transaction)
+    query = <<-SQL
+      SELECT
+        *
+      FROM account_balances
+      WHERE
+        account_id IN ($1, $2)
+        AND EXTRACT(YEAR FROM updated_at) = EXTRACT(YEAR FROM $3::DATE)
+      FOR UPDATE
+    SQL
+    account_balances = AppDatabase.query_all(
+      query,
+      args: [tx.from_account_id, tx.to_account_id, tx.updated_at],
+      as: AccountBalance,
+    )
+
+    account_balances.each do |account_balance|
+      if account_balance.account_id == tx.from_account_id
+        month_update = account_balance.updated_at.month == tx.updated_at.month ? tx.from_amount : 0
+
+        update = {
+          balance:                  account_balance.balance + tx.from_amount,
+          lifetime_additions:       account_balance.lifetime_additions,
+          lifetime_deductions:      account_balance.lifetime_deductions - tx.from_amount,
+          current_month_additions:  account_balance.current_month_additions,
+          current_month_deductions: account_balance.current_month_deductions - month_update,
+          current_year_additions:   account_balance.current_year_additions,
+          current_year_deductions:  account_balance.current_year_deductions - tx.from_amount,
+        }
+      elsif account_balance.account_id == tx.to_account_id
+        month_update = account_balance.updated_at.month == tx.updated_at.month ? tx.to_amount : 0
+
+        update = {
+          balance:                  account_balance.balance - tx.to_amount,
+          lifetime_additions:       account_balance.lifetime_additions - tx.to_amount,
+          lifetime_deductions:      account_balance.lifetime_deductions,
+          current_month_additions:  account_balance.current_month_additions - month_update,
+          current_month_deductions: account_balance.current_month_deductions,
+          current_year_additions:   account_balance.current_year_additions - tx.to_amount,
+          current_year_deductions:  account_balance.current_year_deductions,
+        }
+      else
+        raise "Attempted to update an account balance that doesn't belong to the tx: #{tx.id}"
+      end
+
+      SaveAccountBalance.update!(account_balance, **update)
+    end
+  end
+
   def self.reset_account_balance(account_balance : AccountBalance) : Hash(String, Float64)
     current_date = Time.local
 
