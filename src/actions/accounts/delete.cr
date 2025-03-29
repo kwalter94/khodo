c5b8b319f6c7abd52de180305244b2e9021b32d2
Walter Kaunda <walterkaunda@gmail.com>
2025-03-29 19:34:56 +0200

    Fix redirect error after account deletion

--- src/actions/accounts/delete.cr
+++ src/actions/accounts/delete.cr
@@ -2,8 +2,8 @@ class Accounts::Delete < BrowserAction
   delete "/accounts/:account_id" do
     account = AccountQuery.new.owner_id(current_user.id).find(account_id)
     DeleteAccount.delete(account) do |_operation, _deleted|
-      flash.success = "Deleted the account"
-      redirect Index
+      flash.success = "Deleted account: #{account.name}"
+      redirect Index.path(ledger_id: account.ledger_id)
     end
   end
 end
