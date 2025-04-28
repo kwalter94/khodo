599ea506daed27de689bcd5611fddc5f9f08579c
Walter Kaunda <walterkaunda@gmail.com>
2025-04-28 18:58:08 +0200

    Fix docker files copying bug in deployment script

--- rpi_deploy.sh
+++ rpi_deploy.sh
@@ -17,13 +17,13 @@ docker save -o khodo-$version.img kwalter94/khodo:$version kwalter94/khodo:lates
 
 echo "Copying image to pi"
 ssh $server "[ -d khodo ] || mkdir khodo"
-rsync --progress -r khodo-$version.img docker/pg_init.d/ docker-compose-prod.yml $server:khodo/
+rsync --progress -r khodo-$version.img docker-compose-prod.yml docker $server:khodo/
 ssh $server <<-sh
+  set -x
+
   cd ~/khodo
-  [ -d docker ] || mkdir docker
-  mv pg_init.d docker
   docker load -i khodo-${version}.img
-  TAG=${version} docker-compose -f docker-compose-prod.yml up --wait --detach
+  TAG=${version} docker-compose -f docker-compose-prod.yml up --wait --remove-orphans --detach
   TAG=${version} docker-compose -f docker-compose-prod.yml exec postgres /docker-entrypoint-initdb.d/create-reporting-user.sh
   TAG=${version} docker-compose -f docker-compose-prod.yml restart --no-deps postgres
   rm -f khodo-${version}.img
