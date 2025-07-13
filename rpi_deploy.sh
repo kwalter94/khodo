2c343164eea50c85a8a393f3f184b8de700113dc
Walter Kaunda <walterkaunda@gmail.com>
2025-07-13 16:11:40 +0200

    Limit deployment SSH key decryption prompts to one

--- rpi_deploy.sh
+++ rpi_deploy.sh
@@ -11,11 +11,14 @@ fi
 server=$1
 version=v$(shards version)
 
-echo "Building image"
+echo "Building image..."
 docker buildx build --platform linux/arm64/v8 --load -f docker/Dockerfile -t kwalter94/khodo:latest -t kwalter94/khodo:$version .
 docker save -o khodo-$version.img kwalter94/khodo:$version kwalter94/khodo:latest
 
-echo "Copying image to pi"
+echo "Copying image to pi..."
+eval "$(ssh-agent -s)"
+ssh-add
+
 ssh $server "[ -d khodo ] || mkdir khodo"
 rsync --progress -r khodo-$version.img docker-compose-prod.yml docker $server:khodo/
 ssh $server <<-sh
